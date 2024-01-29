0e26ecf4b2b32ab04ceb690c08f32748855aad0e
Doguhan <DoguhanErolJob@gmail.com>
2024-01-29 18:05:43 +0300

    Get all categories endpoint added.

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import ProductListByCategoryView
+from .views import ProductListByCategoryView,CategoriesView
+
 
 
 urlpatterns = [
     path('v2/kategori/<int:category_id>', ProductListByCategoryView.as_view(), name='product_list_by_category'),
+    path('v2/kategoriler', CategoriesView.as_view(), name='Categories'),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -2,8 +2,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Product
-from .serializers import ProductSerializer
+from .models import Product, Category
+from .serializers import ProductSerializer,CategorySerializer
 
 class ProductListByCategoryView(APIView):
     def get(self, request, category_id):
@@ -12,4 +12,13 @@ class ProductListByCategoryView(APIView):
             serializer = ProductSerializer(products, many=True)
             return Response(serializer.data)
         except Product.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+class CategoriesView(APIView):
+    def get(self, request):
+        try:
+            categorys = Category.objects.all()
+            serializer = CategorySerializer(categorys, many=True)
+            return Response(serializer.data)
+        except Category.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
